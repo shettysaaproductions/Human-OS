633e05a6ec57e33b1970c0273f86642fb7104d3b
shettysaaproductions <samsagar56@gmail.com>
2026-06-28 02:55:56 +0530

    fix: remove broken babel.config.js and metro.config.js (babel-preset-expo not installed)

--- mobile/metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = wrapWithReanimatedMetroConfig(config);
